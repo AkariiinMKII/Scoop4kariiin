81a9abb30e1fc2d9d3487c45d6f1cde3efa940d5
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2023-02-18 01:13:16 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "ScoopInstaller/BucketTemplate:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
