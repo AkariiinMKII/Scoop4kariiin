1f053cb44fdfda9afafdd0caa53cb996c8ff5578
AkariiinMKII <xbloou@gmail.com>
2023-02-18 01:33:55 +0800

    Revert "Update auto-pr.ps1"
    This reverts commit 81a9abb30e1fc2d9d3487c45d6f1cde3efa940d5.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ScoopInstaller/BucketTemplate:master"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
