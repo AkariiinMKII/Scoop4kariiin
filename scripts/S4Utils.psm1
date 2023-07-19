22976fef48c742510c846c48aff1ff77d8695e93
AkariiinMKII <xbloou@gmail.com>
2023-07-19 22:00:00 +0800

    edit scripts

--- scripts/S4Utils.psm1
+++ scripts/S4Utils.psm1
@@ -9,7 +9,7 @@ function New-ProfileModifier {
         Type of scripts to generate.
 
     .PARAMETER PSModuleName
-        Name of PowerShell module, should be $psmodule.name in most situations.
+        Name of PowerShell module, should be $manifest.psmodule.name in most situations.
 
     .PARAMETER AppDir
         Path of the app directory, should be $dir in most situations.
