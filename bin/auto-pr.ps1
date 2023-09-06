d465711e33637bf8e9d95c450d405904cb24c7f8
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2023-09-07 03:41:12 +0800

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "AkariiinMKII/Scoop4kariiin:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
